09690c15970c05d3479bb3dcac8b3bf03d789e8f
Jefté Medeiros <jefte.medeiros8@gmail.com>
2025-07-18 14:20:33 -0300

    done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue'
+
+  const phoneNumber = ref('')
+  const message = ref('')
+
+  const handleWhatsAppRedirect = () => {
+    const baseUrl = 'https://wa.me/'
+    const formattedNumber = phoneNumber.value.replace(/\D/g, '')
+    const encodedMessage = encodeURIComponent(message.value)
+    const whatsappUrl = `${baseUrl}${formattedNumber}${message.value ? `?text=${encodedMessage}` : ''}`
+    window.open(whatsappUrl, '_blank')
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="app-header">
+      <h1>WPP Conversation Starter</h1>
+      <p class="description">Simple app to start conversations without the need to save the contact</p>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="form-container">
+      <div class="input-group">
+        <label for="number">Phone Number</label>
+        <input 
+          type="tel" 
+          id="number"
+          v-model="phoneNumber"
+          placeholder="Enter phone number with country code"
+        />
+      </div>
+
+      <div class="input-group">
+        <label for="message">Message (Optional)</label>
+        <textarea 
+          id="message"
+          v-model="message"
+          placeholder="Type your message here..."
+          rows="3"
+        ></textarea>
+      </div>
+
+      <button @click="handleWhatsAppRedirect" class="start-chat-btn">
+        Start Chat
+      </button>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
